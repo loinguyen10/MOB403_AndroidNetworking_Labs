a8d6ec37b1f93d98d82b473b656cea3ddbfdc790
L. J Nguyen <loinguyengamer2504@gmail.com>
2023-08-05 11:58:14 +0700

    update

--- app/src/main/java/com/ph25590/mob403_labs/lab7/GetData.java
+++ app/src/main/java/com/ph25590/mob403_labs/lab7/GetData.java
@@ -6,56 +6,54 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.ph25590.mob403_labs.sharedFiles.Human;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GetData {
     String kq = "";
-    List<String> list;
 
-    public void getJSONArray(Context context, ListView listView){
+    public void getJSONArray(Context context, RecyclerView recyclerView) {
+        List<Human> list = new ArrayList<>();
         RequestQueue queue = Volley.newRequestQueue(context);
-
-        String url = "https://lmatmet1234.000webhostapp.com/array_json_new.json";
-
-        JsonArrayRequest stringRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+        //b3. url
+        String url = "https://lmatmet1234.000webhostapp.com/api_getSelect.php";
+        //b4. Xac dinh loai request
+        JsonObjectRequest request = new JsonObjectRequest(url, new Response.Listener<JSONObject>() {
             @Override
-            public void onResponse(JSONArray response) {
-                for(int i=0;i< response.length();i++){
-                    try {
-                        JSONObject person = response.getJSONObject(i);
-                        String name = person.getString("name");
-                        String email = person.getString("email");
-                        JSONObject phone = person.getJSONObject("phone");
-                        String mobile = phone.getString("mobile");
-                        String home = phone.getString("home");
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray humans = response.getJSONArray("humans");
+                    for (int i = 0; i < humans.length(); i++) {
+                        JSONObject human = humans.getJSONObject(i);
+                        int id = human.getInt("id");
+                        String name = human.getString("name");
+                        int age = human.getInt("age");
+                        int price = human.getInt("price");
 
-                        Log.d("json: ",name + email + mobile + home);
-                        System.out.println(name + email + mobile + home);
-
-                        kq += "Name: " + name + "\n";
-                        kq += "Email: " + email + "\n";
-                        kq += "Mobile: " + mobile + "\n";
-                        kq += "Home: " + home + "\n";
-
-                        list.add(kq);
-                    }catch (JSONException e){
-                        e.printStackTrace();
+                        list.add(new Human(id,name,age,price));
                     }
-
-                    ArrayAdapter adapter = new ArrayAdapter(context, android.R.layout.simple_list_item_1,list);
-                    listView.setAdapter(adapter);
+                    Adapter adapter = new Adapter(context, (ArrayList<Human>) list);
+                    recyclerView.setAdapter(adapter);
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
             }
         }, new Response.ErrorListener() {
@@ -64,8 +62,36 @@ public class GetData {
                 System.out.println(error.getMessage());
             }
         });
-
-        queue.add(stringRequest);
+        //b5. truyen tham so (neu co)
+        //b6. thuc thi
+        queue.add(request);
     }
 
+    public void updateVolley(Context context, String id, String name, String age, String price) {
+        RequestQueue queue=Volley.newRequestQueue(context);
+        String url="https://lmatmet1234.000webhostapp.com/api_postUpdate.php";
+        StringRequest request=new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        System.out.println(response.toString());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error.getMessage());
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String,String> mydata=new HashMap<>();
+                mydata.put("id",id);
+                mydata.put("name",name);
+                mydata.put("age",age);
+                mydata.put("price",price);
+                return mydata;
+            }
+        };
+        queue.add(request);
+    }
 }
--- app/src/main/java/com/ph25590/mob403_labs/lab7/Lab7Activity.java
+++ app/src/main/java/com/ph25590/mob403_labs/lab7/Lab7Activity.java
@@ -1,36 +1,22 @@
 package com.ph25590.mob403_labs.lab7;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.EditText;
 
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.ph25590.mob403_labs.R;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class Lab7Activity extends AppCompatActivity {
 
     Button button;
-    ListView listView;
-    GetData getData;
+    RecyclerView recyclerView;
+    GetData getData = new GetData();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,16 +24,58 @@ public class Lab7Activity extends AppCompatActivity {
         setContentView(R.layout.activity_lab7);
 
         button = findViewById(R.id.getButton);
-        listView = findViewById(R.id.listView);
+        recyclerView = findViewById(R.id.listView);
 
-        getData = new GetData();
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               getData.getJSONArray(Lab7Activity.this,listView);
+               getData.getJSONArray(Lab7Activity.this,recyclerView);
             }
         });
+
+    }
+
+    public void updateNg(int txtId,String txtName,int txtAge,int txtPrice) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        View v = getLayoutInflater().inflate(R.layout.addmem, null);
+        dialog.setView(v);
+        EditText id = v.findViewById(R.id.edt_id);
+        EditText name = v.findViewById(R.id.edt_name);
+        EditText age = v.findViewById(R.id.edt_age);
+        EditText price = v.findViewById(R.id.edt_price);
+
+        id.setText(txtId+"");
+        name.setText(txtName);
+        age.setText(txtAge+"");
+        price.setText(txtPrice+"");
+
+        id.setEnabled(false);
+
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                updateVolley(
+                        Integer.parseInt(id.getText().toString()),
+                        name.getText().toString(),
+                        Integer.parseInt(age.getText().toString()),
+                        Integer.parseInt(price.getText().toString())
+                );
+            }
+        });
+
+        dialog.setNegativeButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                deleteData(txtId);
+                dialog.create().dismiss();
+            }
+        });
+
+        AlertDialog alert = dialog.create();
+        alert.show();
 
     }
 }
